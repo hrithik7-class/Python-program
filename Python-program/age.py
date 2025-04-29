1db6b9f219474c3bccc4d6fe2902cf428140e3f9
hrithik kesarwani <kesarwaniraj11@gmail.com>
2025-04-29 11:59:57 +0530

    two program code added

---
+++ Python-program/age.py
@@ -0,0 +1,7 @@
+from tkinter import *
+window=Tk()
+window.title("yap9")
+window.geometry("300x300")
+my_label=Label(window,text="python",bg="red",fg="black",font=("times",20),width=20)
+my_label.pack()
+window.mainloop()
